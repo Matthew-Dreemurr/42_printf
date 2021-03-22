a0aa49effd86ff78c4b9bf1407facd8dfaef7a59
Matthew-Dreemurr <hadadmat@gmail.com>
2021-03-22 15:48:48 +0100

    WIP [OK]

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 17:14:09 by mhadad            #+#    #+#             */
-/*   Updated: 2021/03/19 13:24:58 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/03/22 15:48:34 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 #include "include/ft_printf.h"
 #include "include/debug_utils.h"
 #include "include/wraloc.h"
-#define ARG1 "test%%123\n"
+#define ARG1 "test%%123"
 
 int	main(void)
 {
-	printf("pritf ret: [%d]\n", printf(ARG1));
+	int	ret;
+	ret = printf(ARG1);
+	printf(" [%d]\n", ret);
 	puts("\n");
-	printf("ft_pritf ret: [%d]\n", ft_printf(ARG1));
+	ret = 0;
+	ret = ft_printf(ARG1);
+	printf(" [%d]\n", ret);
 	return (0);
 }
