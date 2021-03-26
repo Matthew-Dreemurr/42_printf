6839d7eea3e52e6bfece49bc8eddb65782a3772d
Matthew-Dreemurr <hadadmat@gmail.com>
2021-03-26 17:00:26 +0100

    auto

--- src/convert.c
+++ src/convert.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 16:10:37 by mhadad            #+#    #+#             */
-/*   Updated: 2021/03/26 16:46:17 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/03/26 17:00:11 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	arg_s(t_data *data, va_list *args)
 	const char	*str;
 
 	str = (const char *)va_arg(*args, const char * );
-	if (!str && !*str)
+	if (!str || !*str)
 		return (FALSE);
 	ft_putstr(str, &(*data));
 	data->skip++;
