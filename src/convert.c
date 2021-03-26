9f8103a295d309dad447db0868f8c5d377c87a27
Matthew-Dreemurr <hadadmat@gmail.com>
2021-03-26 17:02:24 +0100

    auto

--- src/convert.c
+++ src/convert.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 16:10:37 by mhadad            #+#    #+#             */
-/*   Updated: 2021/03/26 17:00:11 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/03/26 17:02:19 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,9 @@ int	arg_s(t_data *data, va_list *args)
 
 	str = (const char *)va_arg(*args, const char * );
 	if (!str || !*str)
-		return (FALSE);
-	ft_putstr(str, &(*data));
+		ft_putstr("(null)", &(*data));
+	else
+		ft_putstr(str, &(*data));
 	data->skip++;
 	return (TRUE);
 }
