00dc3189e41309c0220d7ac1af6fc04bd67c8c5a
Matthew-Dreemurr <hadadmat@gmail.com>
2021-04-06 13:48:34 +0200

    Makefile push

--- src/arg_manager.c
+++ src/arg_manager.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 16:10:37 by mhadad            #+#    #+#             */
-/*   Updated: 2021/04/06 13:19:27 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/04/06 13:45:36 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ int	arg_c(t_data *data, va_list *args)
 	char	c;
 
 	c = (char)va_arg(*args, int);
-	if (data->min_width && !data->minus)
+	if (data->min_width && !data->minus && !data->prec)
 		width_print(data->min_width, 1, &(*data));
 	ft_putchar(c, &(*data));
 	data->skip++;
-	if (data->min_width && data->minus)
+	if (data->min_width && data->minus && !data->prec)
 		width_print(data->min_width, 1, &(*data));
 	return (TRUE);
 }
@@ -42,13 +42,13 @@ int	arg_s(t_data *data, va_list *args)
 		len = len_str(str);
 	else
 		len = len_str(STR_NULL);
-	if (data->min_width && !data->minus)
+	if (data->min_width && !data->minus && !data->prec)
 		width_print(data->min_width, len, &(*data));
 	if (!str || !*str)
 		ft_putstr(STR_NULL, &(*data));
 	else
 		ft_putstr(str, &(*data));
-	if (data->min_width && data->minus)
+	if (data->min_width && data->minus && !data->prec)
 		width_print(data->min_width, len, &(*data));
 	data->skip++;
 	return (TRUE);
