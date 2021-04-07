3cac8c6062b33d864fca55314dfd20508f10302e
Matthew-Dreemurr <hadadmat@gmail.com>
2021-04-07 13:40:06 +0200

    Makefile push

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 17:14:09 by mhadad            #+#    #+#             */
-/*   Updated: 2021/04/07 13:33:03 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/04/07 13:38:37 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include "include/debug_utils.h"
 #include "include/wraloc.h"
 
-#define ARG1 "this %s is empty", NULL
+#define ARG1 "%-10.8s", NULL
 int	main(void)
 {
 	int	ret;
