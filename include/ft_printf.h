6e427df627054aafe53bb32b2400337628719a25
Matthew-Dreemurr <hadadmat@gmail.com>
2021-04-15 15:18:48 +0200

    Makefile push 2021-04-15 15:18:48

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 18:35:54 by mhadad            #+#    #+#             */
-/*   Updated: 2021/04/15 15:13:03 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/04/15 15:14:31 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 /* ======  //TODO REMOVE  ======= */
 
-// # include "../include/wraloc.h"
-// # include "../include/debug_utils.h"
-// # include <stdio.h>
+# include "../include/wraloc.h"
+# include "../include/debug_utils.h"
+# include <stdio.h>
 
 /* ======  Include  ======= */
 
