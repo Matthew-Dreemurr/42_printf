72436b18d46aebfe41f8848371b0273e87f9163a
Matthew-Dreemurr <hadadmat@gmail.com>
2021-04-28 13:57:26 +0200

    Makefile push 2021-04-28 13:57:26

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 17:14:09 by mhadad            #+#    #+#             */
-/*   Updated: 2021/04/28 13:55:54 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/04/28 13:56:59 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "include/ft_printf.h"
 #include <limits.h>
 
-#define ARG1 "%.12%"
+#define ARG1 "%23.12%"
 int	main(void)
 {
 	int	ret;
