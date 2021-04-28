b00b7cca633a4b5b3bfc0dff3398a42bcc720530
Matthew-Dreemurr <hadadmat@gmail.com>
2021-04-28 17:30:11 +0200

    Makefile push 2021-04-28 17:30:11

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 17:14:09 by mhadad            #+#    #+#             */
-/*   Updated: 2021/04/28 17:25:39 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/04/28 17:29:39 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "include/ft_printf.h"
 #include <limits.h>
 
-#define ARG1 "ultimate4 %*.*x %*.*X\n", 1, 2, 3, 4, 5, 7
+#define ARG1 "ultimate4 %*.*x %*.*X %*.*x\n", 1, 2, 3, 4, 5, 6, 7, 8, 9
 int	main(void)
 {
 	int	ret;
