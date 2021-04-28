90b32da33982a0d083f493860a3f0e9de93c7061
Matthew-Dreemurr <hadadmat@gmail.com>
2021-04-28 17:40:03 +0200

    Makefile push 2021-04-28 17:40:03

--- src/arg_hex.c
+++ src/arg_hex.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 18:19:47 by mhadad            #+#    #+#             */
-/*   Updated: 2021/04/28 17:36:25 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/04/28 17:39:45 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,7 @@ int	arg_x(const char *str, t_data *data, va_list *args)
 	(void)str;
 	nbr = 0;
 	nbr = (unsigned long)va_arg(*args, unsigned long);
-DE(nbr);
-
+	printf("\n%lu\n\n", nbr);
 data_debug(data);
 	s = ulongtohex(nbr, data);
 	if (!s)
